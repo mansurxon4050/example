d1da48389a1287326c9879648c22d498955e2496
mansurxon4050 <mansurxon4050>
2022-05-06 09:59:31 -0700

    09:59

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
+// api controller
 Route::resource('companies',CompaniesController::class);
 
 Auth::routes();
